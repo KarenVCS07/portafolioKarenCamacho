700af45eeb32dbce7c1560def10e4248532b788d
KarenVCS07 <156975644+KarenVCS07@users.noreply.github.com>
2024-01-24 18:37:56 -0600

    Creacion de proyecto

---
+++ Tienda_KarenCamacho/src/test/java/Tienda/Tienda/TiendaKarenCamachoApplicationTests.java
@@ -0,0 +1,13 @@
+package Tienda.Tienda;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TiendaKarenCamachoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
